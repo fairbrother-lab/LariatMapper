ae4227b229113ad2155a7e3254f565309d1c43e1
truman <85713081+TrumanMooney@users.noreply.github.com>
2024-02-26 11:26:28 -0500

    Refactor to avoid memory error

--- scripts/map_lariats.sh
+++ scripts/map_lariats.sh
@@ -39,19 +39,14 @@ printf "$(date +'%m/%d/%y - %H:%M:%S') | Mapping reads and extracting unmapped r
 output_bam="$OUTPUT_BASE"mapped_reads.bam
 unmapped_bam="$OUTPUT_BASE"unmapped_reads.bam
 bowtie2 --end-to-end --sensitive --score-min L,0,-0.24 -k 1 --n-ceil L,0,0.05 --threads $CPUS -x $GENOME_INDEX -U $READ_FILE \
-	| samtools view --bam --with-header > $output_bam
+	| samtools view --bam --with-header \
+	> $output_bam
 samtools view --bam --with-header --require-flags 4 $output_bam > $unmapped_bam
 mapped_read_count=$(samtools view --count --exclude-flags 4 $output_bam)
 unmapped_read_count=$(samtools view --count $unmapped_bam)
 run_data="$OUTPUT_BASE"run_data.tsv
 echo -e "ref_mapped_reads\t$mapped_read_count" > $run_data
 echo -e "ref_unmapped_reads\t$unmapped_read_count" >> $run_data
-
-### Count pre-mRNA reads
-tagged_bam="$OUTPUT_BASE"mapped_reads_tagged.bam
-bedtools tag -names -i $output_bam -files $INTRONS_BED $EXONS_BED > $tagged_bam
-premRNA_reads="$(python scripts/get_premRNA_reads.py $tagged_bam count)"
-echo -e "premRNA_reads\t$premRNA_reads" >> $run_data
 
 ### Create fasta file of unmapped reads 
 echo ""
@@ -70,7 +65,8 @@ echo ""
 printf "$(date +'%m/%d/%y - %H:%M:%S') | Mapping 5' splice sites to reads...\n"
 fivep_to_reads="$OUTPUT_BASE"fivep_to_reads.sam
 bowtie2 --end-to-end --sensitive --no-unal -f -k 10000 --score-min C,0,0 --threads $CPUS -x $unmapped_fasta -U $FIVEP_FASTA \
-	| samtools view > $fivep_to_reads
+	| samtools view \
+	> $fivep_to_reads
 
 ### Extract reads with a mapped 5' splice site and trim it off
 echo ""
@@ -80,22 +76,18 @@ fivep_info_table="$OUTPUT_BASE"fivep_info_table.tsv
 python scripts/filter_fivep_alignments.py $unmapped_fasta $fivep_to_reads $FIVEP_UPSTREAM $fivep_trimmed_reads $fivep_info_table $OUTPUT_BASE
 
 ### Map 5' trimmed reads to genome
-echo ""
 printf "$(date +'%m/%d/%y - %H:%M:%S') | Mapping 5' trimmed reads to genome...\n"
 trimmed_reads_to_genome="$OUTPUT_BASE"trimmed_reads_to_genome.bam
-bowtie2 --end-to-end --sensitive -k 10 --no-unal --threads $CPUS -f -x $GENOME_INDEX -U $fivep_trimmed_reads \
-	| samtools view --bam --with-header > $trimmed_reads_to_genome
-
-### Get all trimmed alignments that overlap a transcript
-transcript_overlaps="$OUTPUT_BASE"transcript_overlaps.bed
-bedtools intersect -wa -wb -bed -a $trimmed_reads_to_genome -b $TRANSCRIPTS_BED > $transcript_overlaps
-# bedtools tag -names -i $trimmed_reads_to_genome -files $INTRONS_BED $EXONS_BED > $trimmed_reads_to_genome.temp
-# mv $trimmed_reads_to_genome.temp > $trimmed_reads_to_genome
+bowtie2 --end-to-end --very-sensitive -k 10 --no-unal --threads $CPUS -f -x $GENOME_INDEX -U $fivep_trimmed_reads \
+	| samtools view --bam \
+	| bedtools tag -names -f 1 -i - -files $INTRONS_BED \
+	> $trimmed_reads_to_genome
+samtools index $trimmed_reads_to_genome
 
 ### Filter trimmed alignments
 echo ""
 printf "$(date +'%m/%d/%y - %H:%M:%S') | Analyzing trimmed alignments and outputting lariat table...\n"
-python scripts/filter_trimmed_alignments.py $GTF_FILE $TRANSCRIPTS_BED $INTRONS_BED $GENOME_FASTA $OUTPUT_BASE
+python scripts/filter_trimmed_alignments.py $GTF_FILE $INTRONS_BED $GENOME_FASTA $OUTPUT_BASE
 
 ### Filter lariat mappings and choose 1 for each read
 python -u scripts/filter_lariats.py $GTF_FILE $INTRONS_BED $REPEATS_BED $OUTPUT_BASE 
