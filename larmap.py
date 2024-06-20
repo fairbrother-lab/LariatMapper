e7e84f03441d8bf1f82a8772aef5d87bb8f5079f
truman <85713081+TrumanMooney@users.noreply.github.com>
2024-06-20 17:42:06 -0400

    Fix error upon single-end input

--- larmap.py
+++ larmap.py
@@ -16,10 +16,10 @@ from scripts import functions
 # =============================================================================#
 #                                  Functions                                   #
 # =============================================================================#
-def process_args(args:argparse.ArgumentParser):
+def process_args(args:argparse.ArgumentParser, log):
 	# Determine whether input is single-end or paired-end and confirm that the read file(s) exit
 	if args.read_file is not None:
-		seq_type = args.read_file, 'single'
+		seq_type = 'single'
 		if not os.path.isfile(args.read_file):
 			raise ValueError(f'"{args.read_file}" is not an existing file')
 	elif args.read_one is not None and args.read_two is not None:
@@ -81,14 +81,14 @@ if __name__ == '__main__':
 	parser.add_argument('-5', '--ref_5p_fasta', help='FASTA file with sequences of first 20nt of annotated introns')
 	parser.add_argument('-n', '--ref_introns', help='BED file of all annotated introns')
 	# Optional arguments
-	log_levels = parser.add_mutually_exclusive_group()
-	log_levels.add_argument('-q', '--quiet', action='store_true', help="Don't print any status messages")
-	log_levels.add_argument('-d', '--debug', action='store_true', help="Print extensive status messages")
 	parser.add_argument('-m', '--ref_repeatmasker', help='BED file of repetitive regions annotated by RepeatMasker. Putative lariats that map to a repetitive region will be filtered out as false positives (Optional)')
 	parser.add_argument('-t', '--threads', type=int, default=1, help='Number of threads to use for parallel processing (default=1)')
 	parser.add_argument('-p', '--output_prefix', help='Add a prefix to output file names (-o OUT -p ABC   ->   OUT/ABC_lariat_reads.tsv)')
 	parser.add_argument('-u', '--ucsc_track', action='store_true', help='Add an output file named "lariat_reads.bed" which can be used as a custom track in the UCSC Genome Browser (https://www.genome.ucsc.edu/cgi-bin/hgCustom) to visualize lariat alignments')
 	parser.add_argument('-k', '--keep_intermediates', action='store_true', help='Don\'t delete the intermediate files created while running the pipeline (default=delete)')
+	log_levels = parser.add_mutually_exclusive_group()
+	log_levels.add_argument('-q', '--quiet', action='store_true', help="Don't print any status messages")
+	log_levels.add_argument('-d', '--debug', action='store_true', help="Print extensive status messages")
 
 	# Parse
 	args = parser.parse_args()
@@ -107,11 +107,12 @@ if __name__ == '__main__':
 
 	# Print arguments
 	arg_message = [f'{key}={val}' for key, val in vars(args).items() if val is not None and val is not False]
-	arg_message = '\n\t\t\t\t'.join(arg_message)
-	log.info(f'Arguments: {arg_message}')
+	arg_message = '\n\t'.join(arg_message)
+	log.info(f'Arguments: \n\t{arg_message}')
 
 	# Validate the args and determine additional variables
-	args, seq_type, ref_h2index, ref_fasta, ref_5p_fasta, ref_introns, ref_repeatmasker, output_base = process_args(args)
+	args, seq_type, ref_h2index, ref_fasta, ref_5p_fasta, ref_introns, ref_repeatmasker, output_base = process_args(args, log)
+	log.debug(f'seq_type: {repr(seq_type)}, output_base: {repr(output_base)}')
 
 	# Set start method for multiprocessing in filter_fivep_alignments.py and filter_trimmed_alignments.py
 	# Using the default "fork" method causes memory errors when processing bigger RNA-seq inputs
@@ -134,8 +135,8 @@ if __name__ == '__main__':
 		# print(time.strftime('%m/%d/%y - %H:%M:%S | Processing paired-end read files...'), flush=True)
 		log.debug('Processing paired-end read files...')
 		map_lariats_args += [args.read_one, args.read_two]
+	log.debug(f'map_lariats args: {"\n\t".join(map_lariats_args)}')
 
 	# Run it
-	log.debug('Running map_lariats.sh...')
 	map_call = f'{os.path.join(pipeline_dir, "scripts", "map_lariats.sh")} {" ".join(map_lariats_args)}'
 	run(map_call.split(' '))
