4c5a95ce265ef8fe40d7c9658356d5204a5a808b
Truman Mooney <85713081+TrumanMooney@users.noreply.github.com>
2024-08-12 15:50:53 -0400

    Split test input/output into seperate folders by script

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -12,6 +12,10 @@ import pytest
 #                                  Globals                                     #
 # =============================================================================#
 PACKAGE_DIR = pathlib.Path(__file__).parent.parent.resolve()
+BUILD_REFERENCES_DIR = PACKAGE_DIR/'tests'/'build_references'
+FILTER_FIVEP_ALIGNS_DIR = PACKAGE_DIR/'tests'/'filter_fivep_aligns'
+FILTER_HEAD_ALIGNS_DIR = PACKAGE_DIR/'tests'/'filter_head_aligns'
+FILTER_LARIATS_DIR = PACKAGE_DIR/'tests'/'filter_lariats'
 INPUTS_DIR = PACKAGE_DIR/'tests'/'inputs'
 OUTPUTS_DIR = PACKAGE_DIR/'tests'/'outputs'
 
@@ -26,7 +30,7 @@ def load_file_lines(file_a:str, file_b:str, sort:bool=True) -> tuple[list, list]
 	Parameters:
 	- file_a (str): Path to the first file.
 	- file_b (str): Path to the second file.
-	- sort (bool, optional): Whether to sort the lines. Defaults to False.
+	- sort (bool, optional): Whether to sort the lines. Defaults to True.
 	Returns:
 	- lines_a (list): List of lines from file_a.
 	- lines_b (list): List of lines from file_b.
@@ -61,11 +65,7 @@ def vscode_available() -> bool:
 	Returns:
 	- bool: True if available, False otherwise.
 	"""
-	which_check = shutil.which('code')
-	if which_check is None:
-		return False
-	else:
-		return True
+	return shutil.which('code') is not None
 	
 
 def vscode_compare(file_a, file_b):
@@ -84,17 +84,17 @@ def vscode_compare(file_a, file_b):
 # =============================================================================#
 # Required args + the transcript attribute and gene attribute args since their values depend on the genome gtf
 @pytest.mark.parametrize('base',
-					[f"-f {INPUTS_DIR/'genome.fa'} -i {INPUTS_DIR/'hisat2_index'} -x tid -g gid", 
-					f"--genome_fasta {INPUTS_DIR/'genome.fa'}  --hisat2_index {INPUTS_DIR/'hisat2_index'} --transcript_attribute tid --gene_attribute gid",],)
+					[f"-f {BUILD_REFERENCES_DIR/'inputs'/'genome.fa'} -i {BUILD_REFERENCES_DIR/'inputs'/'hisat2_index'} -x tid -g gid", 
+					f"--genome_fasta {BUILD_REFERENCES_DIR/'inputs'/'genome.fa'}  --hisat2_index {BUILD_REFERENCES_DIR/'inputs'/'hisat2_index'} --transcript_attribute tid --gene_attribute gid",],)
 @pytest.mark.parametrize('anno',
-					[f"-a {INPUTS_DIR/'anno.gtf'}", 
-					f"-a {INPUTS_DIR/'anno.gff'}", 
-					f"--genome_anno {INPUTS_DIR/'anno.gtf'}"])
+					[f"-a {BUILD_REFERENCES_DIR/'inputs'/'anno.gtf'}", 
+					f"-a {BUILD_REFERENCES_DIR/'inputs'/'anno.gff'}", 
+					f"--genome_anno {BUILD_REFERENCES_DIR/'inputs'/'anno.gtf'}"])
 # Optional args
 @pytest.mark.parametrize('repeatmasker_bed',
 					[None, 
-					f"-r {INPUTS_DIR/'repeatmasker.bed'}", 
-					f"--repeatmasker_bed {INPUTS_DIR/'repeatmasker.bed'}"])
+					f"-r {BUILD_REFERENCES_DIR/'inputs'/'repeatmasker.bed'}", 
+					f"--repeatmasker_bed {BUILD_REFERENCES_DIR/'inputs'/'repeatmasker.bed'}"])
 @pytest.mark.parametrize('threads',
 					[None, '-t 4', '--threads 4'])
 @pytest.mark.parametrize('copy',
@@ -110,9 +110,9 @@ def test_build_references(base, anno, repeatmasker_bed, threads, copy, tmp_path)
 	assert response.returncode == 0, response_text
 
 	# Check output
-	for ref, out in ((INPUTS_DIR/'exons.tsv', tmp_path/'exons.tsv.gz'),
-					(INPUTS_DIR/'introns.tsv', tmp_path/'introns.tsv.gz'),
-					(INPUTS_DIR/'fivep_sites.fa', tmp_path/'fivep_sites.fa')):
+	for ref, out in ((BUILD_REFERENCES_DIR/'outputs'/'exons.tsv', tmp_path/'exons.tsv.gz'),
+					(BUILD_REFERENCES_DIR/'outputs'/'introns.tsv', tmp_path/'introns.tsv.gz'),
+					(BUILD_REFERENCES_DIR/'outputs'/'fivep_sites.fa', tmp_path/'fivep_sites.fa')):
 		ref_lines, out_lines = load_file_lines(ref, out)
 		if ref_lines == out_lines:
 			continue
@@ -129,23 +129,22 @@ def test_build_references(base, anno, repeatmasker_bed, threads, copy, tmp_path)
 						['1', '2', '3', '4', '5'])
 @pytest.mark.parametrize('prefix',
 						['', 'prefix_'])
-@pytest.mark.parametrize('seq_type', 
-						 ['paired'])
-def test_filter_fivep_aligns(threads, prefix, seq_type, tmp_path):
+def test_filter_fivep_aligns(threads, prefix, tmp_path):
 	# Link files needed in working dir
 	for file in ('unmapped_reads.fa', 'unmapped_reads.fa.fai', 'fivep_to_reads.sam', 'fivep_sites.fa'):
-		os.symlink(OUTPUTS_DIR/seq_type/file, tmp_path/f'{prefix}{file}')
+		os.symlink(FILTER_FIVEP_ALIGNS_DIR/'inputs'/file, tmp_path/f'{prefix}{file}')
 
 	# Run script
 	command = f"python {PACKAGE_DIR/'scripts'/'filter_fivep_aligns.py'} {threads}" \
-			  f" {INPUTS_DIR/'genome.fa'} {INPUTS_DIR/'fivep_sites.fa'} {tmp_path}/{prefix} DEBUG"
+			  f" {FILTER_FIVEP_ALIGNS_DIR/'inputs'/'genome.fa'} {FILTER_FIVEP_ALIGNS_DIR/'inputs'/'fivep_sites.fa'} {tmp_path}/{prefix} DEBUG"
 	response = subprocess.run(command, shell=True, capture_output=True, text=True)
 	response_text = '\n' + response.stdout + response.stderr
 	assert response.returncode == 0, response_text
 
 	# Check output
-	for ref, out in ((OUTPUTS_DIR/seq_type/'failed_fivep_alignments.tsv', tmp_path/f'{prefix}failed_fivep_alignments.tsv'),
-				  	(OUTPUTS_DIR/seq_type/'tails.tsv', tmp_path/f'{prefix}tails.tsv')):
+	for ref, out in ((FILTER_FIVEP_ALIGNS_DIR/'outputs'/'failed_fivep_alignments.tsv', tmp_path/f'{prefix}failed_fivep_alignments.tsv'),
+				  	(FILTER_FIVEP_ALIGNS_DIR/'outputs'/'tails.tsv', tmp_path/f'{prefix}tails.tsv'),
+				  	(FILTER_FIVEP_ALIGNS_DIR/'outputs'/'heads.fa', tmp_path/f'{prefix}heads.fa')):
 		ref_lines, out_lines = load_file_lines(ref, out)
 		if ref_lines == out_lines:
 			continue
@@ -162,25 +161,23 @@ def test_filter_fivep_aligns(threads, prefix, seq_type, tmp_path):
 						['1', '2', '3', '4', '5'])
 @pytest.mark.parametrize('prefix',
 						['', 'prefix_'])
-@pytest.mark.parametrize('seq_type', 
-						 ['paired'])
-def test_filter_head_aligns(threads, prefix, seq_type, tmp_path):
+def test_filter_head_aligns(threads, prefix, tmp_path):
 	# Link files needed in working dir
 	for file in ('heads_to_genome.sam', 'tails.tsv'):
-		os.symlink(OUTPUTS_DIR/seq_type/file, tmp_path/f'{prefix}{file}')
+		os.symlink(FILTER_HEAD_ALIGNS_DIR/'inputs'/file, tmp_path/f'{prefix}{file}')
 
 	# Run script
 	command = f"python {PACKAGE_DIR/'scripts'/'filter_head_aligns.py'} {threads}" \
-			  f" {INPUTS_DIR/'introns.tsv'} {INPUTS_DIR/'genome.fa'} {tmp_path}/{prefix} DEBUG"  
+			  f" {FILTER_HEAD_ALIGNS_DIR/'inputs'/'introns.tsv'} {FILTER_HEAD_ALIGNS_DIR/'inputs'/'genome.fa'} {tmp_path}/{prefix} DEBUG"  
 	response = subprocess.run(command, shell=True, capture_output=True, text=True)
 	response_text = '\n' + response.stdout + response.stderr
 	assert response.returncode == 0, response_text
 
 	# Check output
-	for ref, out in ((OUTPUTS_DIR/seq_type/'failed_head_alignments.tsv', tmp_path/f'{prefix}failed_head_alignments.tsv'),
-					(OUTPUTS_DIR/seq_type/'template_switching_reads.tsv', tmp_path/f'{prefix}template_switching_reads.tsv'),
-					(OUTPUTS_DIR/seq_type/'circularized_intron_reads.tsv', tmp_path/f'{prefix}circularized_intron_reads.tsv'),
-					(OUTPUTS_DIR/seq_type/'putative_lariats.tsv', tmp_path/f'{prefix}putative_lariats.tsv')):
+	for ref, out in ((FILTER_HEAD_ALIGNS_DIR/'outputs'/'failed_head_alignments.tsv', tmp_path/f'{prefix}failed_head_alignments.tsv'),
+					(FILTER_HEAD_ALIGNS_DIR/'outputs'/'template_switching_reads.tsv', tmp_path/f'{prefix}template_switching_reads.tsv'),
+					(FILTER_HEAD_ALIGNS_DIR/'outputs'/'circularized_intron_reads.tsv', tmp_path/f'{prefix}circularized_intron_reads.tsv'),
+					(FILTER_HEAD_ALIGNS_DIR/'outputs'/'putative_lariats.tsv', tmp_path/f'{prefix}putative_lariats.tsv')):
 		ref_lines, out_lines = load_file_lines(ref, out)
 		if ref_lines == out_lines:
 			continue
@@ -201,18 +198,18 @@ def test_filter_head_aligns(threads, prefix, seq_type, tmp_path):
 def test_filter_lariats(prefix, seq_type, tmp_path):
 	# Link files needed in working dir
 	for file in ('output.bam', 'template_switching_reads.tsv', 'circularized_intron_reads.tsv', 'putative_lariats.tsv'):
-		os.symlink(OUTPUTS_DIR/seq_type/file, tmp_path/f'{prefix}{file}')
+		os.symlink(FILTER_LARIATS_DIR/'inputs'/file, tmp_path/f'{prefix}{file}')
 
 	# Run script
 	command = f"python {PACKAGE_DIR/'scripts'/'filter_lariats.py'} {tmp_path}/{prefix} DEBUG " \
-			  f" {seq_type} {INPUTS_DIR/'genome.fa'} {INPUTS_DIR/'repeatmasker.bed'}" 
+			  f" {seq_type} {FILTER_LARIATS_DIR/'inputs'/'genome.fa'} {FILTER_LARIATS_DIR/'inputs'/'repeatmasker.bed'}" 
 	response = subprocess.run(command, shell=True, capture_output=True, text=True)
 	response_text = '\n' + response.stdout + response.stderr
 	assert response.returncode == 0, response_text
 
 	# Check output
-	for ref, out in ((OUTPUTS_DIR/seq_type/'failed_lariat_alignments.tsv', tmp_path/f'{prefix}failed_lariat_alignments.tsv'),
-					(OUTPUTS_DIR/seq_type/'lariat_reads.tsv', tmp_path/f'{prefix}lariat_reads.tsv')):
+	for ref, out in ((FILTER_LARIATS_DIR/'outputs'/'failed_lariat_alignments.tsv', tmp_path/f'{prefix}failed_lariat_alignments.tsv'),
+					(FILTER_LARIATS_DIR/'outputs'/'lariat_reads.tsv', tmp_path/f'{prefix}lariat_reads.tsv')):
 		ref_lines, out_lines = load_file_lines(ref, out)
 		if ref_lines == out_lines:
 			continue
