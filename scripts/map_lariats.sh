c701583bfa98dd19847078122359575c6ed0e3f7
Truman Mooney <85713081+TrumanMooney@users.noreply.github.com>
2024-08-28 13:04:22 -0400

    larmap, map_lariats: add custom Settings dataclass to hold arguments and run settings

--- scripts/map_lariats.sh
+++ scripts/map_lariats.sh
@@ -34,15 +34,7 @@ KEEP_CLASSES="${13}"
 # Keep the temp files created during the run. true or false, default false
 KEEP_TEMP="${14}"
 # RNA-seq fastq read file(s)
-if [ "$SEQ_TYPE" == "single" ]; then
-	READ_FILE="${15}"
-elif [ "$SEQ_TYPE" == "paired" ]; then
-	READ_ONE="${15}"
-	READ_TWO="${16}"
-else
-	echo "SEQ_TYPE '$SEQ_TYPE' not recognized"
-	exit 1
-fi
+INPUT_FILES="${15}"
 
 
 
@@ -63,88 +55,90 @@ failed_heads="$OUTPUT_BASE"failed_head_alignments.tsv
 failed_lariat="$OUTPUT_BASE"failed_lariat_alignments.tsv
 
 
-
 #=============================================================================#
 #                                    Calls                                    #
 #=============================================================================#
-# ### Map filtered reads to genome and keep unmapped reads. Lariat reads crossing the brachpoint will not be able to map to the gene they're from
-# printf "$(date +'%d/%b/%y %H:%M:%S') | Mapping reads to genome...\n"
-# if [ "$SEQ_TYPE" == "single" ]; then
-# 	hisat2 --no-softclip -k 1 --max-seeds 20 --pen-noncansplice 0 --n-ceil L,0,0.05 --score-min L,0,-0.24 --bowtie2-dp 1 \
-# 	       --threads $THREADS -x $GENOME_INDEX -U $READ_FILE \
-# 		| samtools view --bam --with-header --add-flags PAIRED,READ1 \
-# 		| samtools sort --threads $THREADS --verbosity 0 \
-# 		> $output_bam \
-# 		|| exit 1
-# elif [ "$SEQ_TYPE" == "paired" ]; then
-# 	hisat2 --no-softclip -k 1 --max-seeds 20 --pen-noncansplice 0 --n-ceil L,0,0.05 --score-min L,0,-0.24 --bowtie2-dp 1 \
-# 		   --threads $THREADS -x $GENOME_INDEX -1 $READ_ONE -2 $READ_TWO \
-# 		| samtools view --bam --with-header \
-# 		| samtools sort --threads $THREADS --verbosity 0 \
-# 		> $output_bam \
-# 		|| exit 1
-# fi
-# samtools index $output_bam
+### Map filtered reads to genome and keep unmapped reads. Lariat reads crossing the brachpoint will not be able to map to the gene they're from
+printf "$(date +'%d/%b/%y %H:%M:%S') | Mapping reads to genome...\n"
+if [ "$SEQ_TYPE" == "single" ]; then
+	hisat2 --no-softclip -k 1 --max-seeds 20 --pen-noncansplice 0 --n-ceil L,0,0.05 --score-min L,0,-0.24 --bowtie2-dp 1 \
+	       --threads $THREADS -x $GENOME_INDEX -U $INPUT_FILES \
+		| samtools view --bam --with-header --add-flags PAIRED,READ1 \
+		| samtools sort --threads $THREADS --verbosity 0 \
+		> $output_bam \
+		|| exit 1
+elif [ "$SEQ_TYPE" == "paired" ]; then
+	# Get the two read files from the comma-separated list
+	IFS=',' read -r read_one read_two <<< "$INPUT_FILES"
+	# Map
+	hisat2 --no-softclip -k 1 --max-seeds 20 --pen-noncansplice 0 --n-ceil L,0,0.05 --score-min L,0,-0.24 --bowtie2-dp 1 \
+		   --threads $THREADS -x $GENOME_INDEX -1 $read_one -2 $read_two \
+		| samtools view --bam --with-header \
+		| samtools sort --threads $THREADS --verbosity 0 \
+		> $output_bam \
+		|| exit 1
+fi
+samtools index $output_bam
 
-# unmapped_read_count=$(samtools view --count --require-flags 4 $output_bam)
-# if [ $unmapped_read_count == 0 ];then
-# 	printf "$(date +'%d/%b/%y %H:%M:%S') | No reads remaining\n"
-# 	python -u $PIPELINE_DIR/scripts/classify_linear.py $OUTPUT_BASE $EXONS_TSV $INTRONS_TSV $SEQ_TYPE $LOG_LEVEL \
-# 		|| exit 1
-# 	python -u $PIPELINE_DIR/scripts/classify_nonlinear.py $OUTPUT_BASE $SEQ_TYPE $LOG_LEVEL \
-# 		|| exit 1
-# 	python -u $PIPELINE_DIR/scripts/summarise.py $OUTPUT_BASE $LOG_LEVEL $SEQ_TYPE \
-# 		|| exit 1
-# fi
+unmapped_read_count=$(samtools view --count --require-flags 4 $output_bam)
+if [ $unmapped_read_count == 0 ];then
+	printf "$(date +'%d/%b/%y %H:%M:%S') | No reads remaining\n"
+	python -u $PIPELINE_DIR/scripts/classify_linear.py $OUTPUT_BASE $EXONS_TSV $INTRONS_TSV $SEQ_TYPE $LOG_LEVEL \
+		|| exit 1
+	python -u $PIPELINE_DIR/scripts/classify_nonlinear.py $OUTPUT_BASE $SEQ_TYPE $LOG_LEVEL \
+		|| exit 1
+	python -u $PIPELINE_DIR/scripts/summarise.py $OUTPUT_BASE $LOG_LEVEL $SEQ_TYPE \
+		|| exit 1
+fi
 
-# ### Create fasta file of unmapped reads 
-# printf "$(date +'%d/%b/%y %H:%M:%S') | Creating fasta file of unmapped reads...\n"
-# samtools fasta -N --require-flags 4 -o $unmapped_fasta $output_bam >/dev/null 2>&1 || exit 1
-# printf "$(date +'%d/%b/%y %H:%M:%S') | Indexing unmapped reads fasta file...\n"
-# samtools faidx $unmapped_fasta || exit 1
+### Create fasta file of unmapped reads 
+printf "$(date +'%d/%b/%y %H:%M:%S') | Creating fasta file of unmapped reads...\n"
+samtools fasta -N --require-flags 4 -o $unmapped_fasta $output_bam >/dev/null 2>&1 || exit 1
+printf "$(date +'%d/%b/%y %H:%M:%S') | Indexing unmapped reads fasta file...\n"
+samtools faidx $unmapped_fasta || exit 1
 
-# ### Build a bowtie2 index of the unmapped reads
-# printf "$(date +'%d/%b/%y %H:%M:%S') | Building bowtie2 index of unmapped fasta...\n"
-# bowtie2-build --quiet --threads $THREADS $unmapped_fasta $unmapped_fasta || exit 1 
+### Build a bowtie2 index of the unmapped reads
+printf "$(date +'%d/%b/%y %H:%M:%S') | Building bowtie2 index of unmapped fasta...\n"
+bowtie2-build --quiet --threads $THREADS $unmapped_fasta $unmapped_fasta || exit 1 
 
-# ## Align fasta file of all 5' splice sites (first 20nts of introns) to unmapped reads index
-# # We need to order the output SAM by reference (the read id, in this case) for the following filtering process
+## Align fasta file of all 5' splice sites (first 20nts of introns) to unmapped reads index
+# We need to order the output SAM by reference (the read id, in this case) for the following filtering process
+printf "$(date +'%d/%b/%y %H:%M:%S') | Mapping 5' splice sites to reads...\n"
+bowtie2 --end-to-end --sensitive --no-unal -f -k 10000 --score-min C,0,0 --threads $THREADS -x $unmapped_fasta -U $FIVEP_FASTA \
+	| samtools sort --threads $THREADS --verbosity 0 --output-fmt SAM -M \
+	| samtools view \
+	> $fivep_to_reads \
+	|| exit 1 
+
+# # ## Align unmapped reads to index of all 5' splice sites (first 20nts of introns)
 # printf "$(date +'%d/%b/%y %H:%M:%S') | Mapping 5' splice sites to reads...\n"
-# bowtie2 --end-to-end --sensitive --no-unal -f -k 10000 --score-min C,0,0 --threads $THREADS -x $unmapped_fasta -U $FIVEP_FASTA \
-# 	| samtools sort --threads $THREADS --verbosity 0 --output-fmt SAM -M \
-# 	| samtools view \
-# 	> $fivep_to_reads \
-# 	|| exit 1 
+# bowtie2 --local -k 1000 -L 20 -i C,1,0 --ma 1 --mp 1,1 --np 1 --rdg 1,1 --rfg 1,1 --score-min C,20,0 \
+# 		--no-unal --no-head --threads $THREADS -x $FIVEP_INDEX -f -U $unmapped_fasta \
+# 	> $reads_to_fivep 
 
-# # # ## Align unmapped reads to index of all 5' splice sites (first 20nts of introns)
-# # printf "$(date +'%d/%b/%y %H:%M:%S') | Mapping 5' splice sites to reads...\n"
-# # bowtie2 --local -k 1000 -L 20 -i C,1,0 --ma 1 --mp 1,1 --np 1 --rdg 1,1 --rfg 1,1 --score-min C,20,0 \
-# # 		--no-unal --no-head --threads $THREADS -x $FIVEP_INDEX -f -U $unmapped_fasta \
-# # 	> $reads_to_fivep 
+## Extract reads with a mapped 5' splice site and trim it off
+printf "$(date +'%d/%b/%y %H:%M:%S') | Finding 5' read alignments and trimming reads...\n"
+python -u $PIPELINE_DIR/scripts/filter_fivep_aligns.py $THREADS $GENOME_FASTA $FIVEP_FASTA $OUTPUT_BASE $LOG_LEVEL \
+	|| exit 1 
 
-# ## Extract reads with a mapped 5' splice site and trim it off
-# printf "$(date +'%d/%b/%y %H:%M:%S') | Finding 5' read alignments and trimming reads...\n"
-# python -u $PIPELINE_DIR/scripts/filter_fivep_aligns.py $THREADS $GENOME_FASTA $FIVEP_FASTA $OUTPUT_BASE $LOG_LEVEL \
-# 	|| exit 1 
+### Map read heads to genome
+printf "$(date +'%d/%b/%y %H:%M:%S') | Mapping heads to genome...\n"
+hisat2 --no-softclip --no-spliced-alignment --very-sensitive -k 100 \
+	   --no-unal --threads $THREADS -f -x $GENOME_INDEX -U $heads_fasta \
+	| samtools sort --threads $THREADS --verbosity 0 --output-fmt SAM -n \
+	| samtools view \
+	> $heads_to_genome \
+	|| exit 1
 
-# ### Map read heads to genome
-# printf "$(date +'%d/%b/%y %H:%M:%S') | Mapping heads to genome...\n"
-# hisat2 --no-softclip --no-spliced-alignment --very-sensitive -k 100 \
-# 	   --no-unal --threads $THREADS -f -x $GENOME_INDEX -U $heads_fasta \
-# 	| samtools sort --threads $THREADS --verbosity 0 --output-fmt SAM -n \
-# 	| samtools view \
-# 	> $heads_to_genome \
-# 	|| exit 1
+### Filter head alignments
+printf "$(date +'%d/%b/%y %H:%M:%S') | Analyzing head alignments and outputting lariat table...\n"
+python -u $PIPELINE_DIR/scripts/filter_head_aligns.py $THREADS $INTRONS_TSV $GENOME_FASTA $OUTPUT_BASE $LOG_LEVEL \
+	|| exit 1 
 
-# ### Filter head alignments
-# printf "$(date +'%d/%b/%y %H:%M:%S') | Analyzing head alignments and outputting lariat table...\n"
-# python -u $PIPELINE_DIR/scripts/filter_head_aligns.py $THREADS $INTRONS_TSV $GENOME_FASTA $OUTPUT_BASE $LOG_LEVEL \
-# 	|| exit 1 
-
-# ### Filter lariat mappings and choose 1 for each read
-# printf "$(date +'%d/%b/%y %H:%M:%S') | Filtering putative lariat alignments...\n"
-# python -u $PIPELINE_DIR/scripts/filter_lariats.py $OUTPUT_BASE $LOG_LEVEL $SEQ_TYPE $GENOME_FASTA $REPEATS_BED \
-# 	|| exit 1 
+### Filter lariat mappings and choose 1 for each read
+printf "$(date +'%d/%b/%y %H:%M:%S') | Filtering putative lariat alignments...\n"
+python -u $PIPELINE_DIR/scripts/filter_lariats.py $OUTPUT_BASE $LOG_LEVEL $SEQ_TYPE $GENOME_FASTA $REPEATS_BED \
+	|| exit 1 
 
 ### Classify reads
 python -u $PIPELINE_DIR/scripts/classify_linear.py $OUTPUT_BASE $EXONS_TSV $INTRONS_TSV $SEQ_TYPE $LOG_LEVEL \
