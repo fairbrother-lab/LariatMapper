67569e0d606dc1da08a8d5299560ff07b51f8e9b
Truman Mooney <85713081+TrumanMooney@users.noreply.github.com>
2024-09-09 14:41:21 -0400

    Remove classify_linear step for taking way too long to finish for an interim solution

--- scripts/map_lariats.sh
+++ scripts/map_lariats.sh
@@ -97,8 +97,6 @@ samtools index $output_bam
 unmapped_read_count=$(samtools view --count --require-flags 4 $output_bam)
 if [ $unmapped_read_count == 0 ];then
 	printf "$(date +'%d/%b/%y %H:%M:%S') | No reads remaining\n"
-	python -W ignore $PIPELINE_DIR/scripts/classify_linear.py $OUTPUT_BASE $EXONS_TSV $INTRONS_TSV $SEQ_TYPE $LOG_LEVEL \
-		|| exit 1
 	python -u $PIPELINE_DIR/scripts/classify_nonlinear.py $OUTPUT_BASE $SEQ_TYPE $LOG_LEVEL \
 		|| exit 1
 	python -u $PIPELINE_DIR/scripts/summarise.py $OUTPUT_BASE $LOG_LEVEL $SEQ_TYPE \
@@ -155,8 +153,6 @@ python -u $PIPELINE_DIR/scripts/filter_lariats.py $OUTPUT_BASE $LOG_LEVEL $SEQ_T
 	|| exit 1 
 
 ### Classify reads
-python -W ignore $PIPELINE_DIR/scripts/classify_linear.py $OUTPUT_BASE $EXONS_TSV $INTRONS_TSV $SEQ_TYPE $LOG_LEVEL \
-	|| exit 1
 python -u $PIPELINE_DIR/scripts/classify_nonlinear.py $OUTPUT_BASE $SEQ_TYPE $LOG_LEVEL \
 	|| exit 1
 python -u $PIPELINE_DIR/scripts/summarise.py $OUTPUT_BASE $LOG_LEVEL $SEQ_TYPE \
