cf3e4398759a54460c55a68ae86502aad363594b
Truman Mooney <85713081+TrumanMooney@users.noreply.github.com>
2024-09-23 13:50:40 -0400

    summarise: add lariat read bp statsitics to summary.txt

--- scripts/summarise.py
+++ scripts/summarise.py
@@ -70,6 +70,17 @@ SUMMARY_TEMPLATE = (
 					"Head mapping:\t{Head_mapping}\n"
 					"Head alignment filtering:\t{Head_alignment_filtering}\n"
 					"Lariat filtering:\t{Lariat_filtering}\n"
+					"\n"
+					"----------------------------------------\n"
+					"          Additional statistics         \n"
+					"----------------------------------------\n"
+					"Lariat reads, genomic_bp_nt = A:\t{A:.2%}\n"
+					"Lariat reads, genomic_bp_nt = C:\t{C:.2%}\n"
+					"Lariat reads, genomic_bp_nt = G:\t{G:.2%}\n"
+					"Lariat reads, genomic_bp_nt = T:\t{T:.2%}\n"
+					"Lariat reads, genomic_bp_nt = N:\t{N:.2%}\n"
+					"Lariat reads, genomic_bp_nt ≠ read_bp_nt:\t{bp_mismatch:.2%}\n"
+					"Lariat reads, |bp_dist_to_threep| ≤ 70:\t{within_70:.2%}\n"
 )
 READ_COUNTS_TEMPLATE = (
 						"Category\tSubcategory\tReads\n"
@@ -188,6 +199,14 @@ if __name__ == '__main__':
 			if align.is_mapped and align.mate_is_unmapped:
 				mp += 1
 		stats['mixed_pairs'] = mp
+
+	# Add additional info
+	lariat_reads = pd.read_csv(f'{output_base}lariat_reads.tsv', sep='\t')
+	lariat_reads.genomic_bp_nt = pd.Categorical(lariat_reads.genomic_bp_nt, categories=['A', 'C', 'G', 'T', 'N'])
+	stats.update(lariat_reads.genomic_bp_nt.value_counts(normalize=True).to_dict())
+	stats['within_70'] = lariat_reads.bp_dist_to_threep.abs().le(70).sum()/lariat_reads.shape[0]
+	stats['bp_mismatch'] = lariat_reads.genomic_bp_nt.ne(lariat_reads.read_bp_nt).sum()/lariat_reads.shape[0]
+	
 	
 	# Write summary info to file
 	log.debug(f'Summary stats: {stats}')
