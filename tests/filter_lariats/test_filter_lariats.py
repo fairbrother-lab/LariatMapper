5f9694dfb1aafdaa265c68c1d6c3ae657f65a803
Truman Mooney <85713081+TrumanMooney@users.noreply.github.com>
2024-10-15 12:29:59 -0400

    tests: move testing functions from one file into individual files to allow individual script testing

---
+++ tests/filter_lariats/test_filter_lariats.py
@@ -0,0 +1,57 @@
+import gzip
+import os
+import pathlib
+import shutil
+import subprocess
+import sys
+
+import pytest
+
+sys.path.append(str(pathlib.Path(__file__).parent.parent.resolve()))
+import testing_tools as test_funcs
+
+
+
+# =============================================================================#
+#                                  Globals                                     #
+# =============================================================================#
+PACKAGE_DIR = pathlib.Path(__file__).parent.parent.parent.resolve()
+FILTER_LARIATS_DIR = PACKAGE_DIR/'tests'/'filter_lariats'
+INPUTS_DIR = PACKAGE_DIR/'tests'/'inputs'
+OUTPUTS_DIR = PACKAGE_DIR/'tests'/'outputs'
+
+
+
+# =============================================================================#
+#                                   Tests                                      #
+# =============================================================================#
+@pytest.mark.parametrize('prefix',
+						['', 'prefix_'])
+@pytest.mark.parametrize('seq_type', 
+						 ['paired'])
+def test_filter_lariats(prefix, seq_type, tmp_path):
+	# Link files needed in working dir
+	for file in ('output.bam', 'template_switching_reads.tsv', 'circularized_intron_reads.tsv', 'putative_lariats.tsv'):
+		os.symlink(FILTER_LARIATS_DIR/'inputs'/file, tmp_path/f'{prefix}{file}')
+
+	# Run script
+	command = f"python {PACKAGE_DIR/'scripts'/'filter_lariats.py'} {tmp_path}/{prefix} DEBUG" \
+			  f" {seq_type} {FILTER_LARIATS_DIR/'inputs'/'genome.fa'} {FILTER_LARIATS_DIR/'inputs'/'repeatmasker.bed'}" 
+	response = subprocess.run(command, shell=True, capture_output=True, text=True)
+	response_text = '\n' + response.stdout + response.stderr
+	assert response.returncode == 0, response_text
+
+	# Check output
+	for ref, out in ((FILTER_LARIATS_DIR/'outputs'/'failed_lariat_alignments.tsv', tmp_path/f'{prefix}failed_lariat_alignments.tsv'),
+					(FILTER_LARIATS_DIR/'outputs'/'lariat_reads.tsv', tmp_path/f'{prefix}lariat_reads.tsv')):
+		ref_lines, out_lines = test_funcs.load_file_lines(ref, out)
+		if ref_lines == out_lines:
+			continue
+
+		# File contents differ, decide how to report
+		if test_funcs.vscode_available():
+			test_funcs.vscode_compare(ref, out)
+			print(response_text)
+			pytest.fail(f'Output file differs from expected output: {out.name}')
+		else:
+			assert ref_lines == out_lines, response_text
