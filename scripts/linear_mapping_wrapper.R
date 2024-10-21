4771cf9f74fbc580f5e263b2eea9d9c4f4269f87
Truman Mooney <85713081+TrumanMooney@users.noreply.github.com>
2024-10-21 15:28:16 -0400

    linear_mapping_wrapper: change seq_end names to single and paired

--- scripts/linear_mapping_wrapper.R
+++ scripts/linear_mapping_wrapper.R
@@ -14,8 +14,8 @@ option_list <- list(
                 help = "Path to GTF dir", metavar = "DIR_PATH"),
     make_option(c("-o", "--output"), type = "character", default = NULL, 
                 help = "Path to output dir", metavar = "OUT_PATH"),
-    make_option(c("-r", "--read_layout"), type = "character", default = "pairEnd", 
-                help = "singleEnd or pairEnd datasets", metavar = "READ_LAYOUT")
+    make_option(c("-r", "--read_layout"), type = "character", default = "paired", 
+                help = "single = single-end sequencing data, paired = paired-end sequencing data", metavar = "READ_LAYOUT")
 )
 
 parser <- OptionParser(option_list = option_list)
@@ -27,8 +27,8 @@ gtf_dir <- opts$gtf
 output_dir <- opts$output
 read_layout <- opts$read_layout
 
-if (is.null(read_layout) || !read_layout %in% c("singleEnd", "pairEnd")) {
-    stop("--read_layout must be singleEnd or pairEnd.")
+if (is.null(read_layout) || !read_layout %in% c("single", "paired")) {
+    stop("--read_layout must be 'single' or 'paired'.")
 }
 
 ### Source utils
@@ -42,7 +42,7 @@ intron_gr <- readRDS(file.path(gtf_dir, "intron_gr.rds"))
 ###
 
 ### Run counting step
-singleEnd_mode <- ifelse(read_layout == "pairEnd", F, T)
+singleEnd_mode <- ifelse(read_layout == "paired", F, T)
 bam_res <- linear_map_integrate(input_bam,
                                 gene_gr = gene_gr, exon_gr = exon_gr, intron_gr = intron_gr,
                                 singleEnd = singleEnd_mode)
@@ -62,3 +62,119 @@ write.table(bam_res, file.path(output_dir,
                                paste0(basename(input_bam), "_", "count.tsv")),
             row.names = F, col.names = T, quote = F, sep = "\t")
 ###
+
+
+library(magrittr)
+library(rtracklayer)
+library(GenomicFeatures)
+
+# dir_path <- "/users/yzhong36/data_rsingh47/yzhong36/BP_prediction/lariat_simulation/linear_mapping/ref"
+
+# # Check if the directory exists
+# if (!file.exists(dir_path)) {
+#   # Create the directory
+#   dir.create(dir_path, recursive = TRUE)
+#   message("Directory created: ", dir_path)
+# } else {
+#   message("Directory already exists: ", dir_path)
+# }
+
+# ### prepare annotation file
+# gtf <- import("/users/yzhong36/data_rsingh47/yzhong36/BP_prediction/datasets/annotations/gencode/gencode.v44.basic.annotation.gtf.gz") 
+# gtf
+# table(gtf$gene_type)
+
+# # BAD_GENE_TYPES = c('transcribed_unprocessed_pseudogene',
+# #                    'unprocessed_pseudogene',
+# #                    'processed_pseudogene' ,
+# #                    'transcribed_processed_pseudogene' ,
+# #                    'TEC',
+# #                    'transcribed_unitary_pseudogene' ,
+# #                    'rRNA_pseudogene',
+# #                    'unitary_pseudogene' ,
+# #                    'pseudogene' ,
+# #                    'IG_V_pseudogene' ,
+# #                    'TR_V_pseudogene' ,
+# #                    'IG_C_pseudogene',
+# #                    'TR_J_pseudogene' ,
+# #                    'IG_J_pseudogene' ,
+# #                    'IG_pseudogene',
+# #                    'artifact' 
+# #                    )
+
+# BAD_GENE_TYPES <- c()
+
+# filtered_gtf <- gtf[!gtf$gene_type %in% BAD_GENE_TYPES]
+# table(filtered_gtf$gene_type)
+
+# txdb <- makeTxDbFromGRanges(filtered_gtf)
+# ###
+
+# Get args
+args <- commandArgs(trailingOnly = TRUE)
+
+# Define arg options
+option_list <- list(
+    make_option(c("-a", "--anno"), type = "character", default = NULL, 
+                help = "input annotation file (GTF or GFF)", metavar = "ANNO_PATH"),
+	make_option(c("-o", "--output"), type = "character", default = NULL, 
+                help = "Path to output dir", metavar = "OUT_PATH"),
+	make_option(c("-d", "--quiet"), action = "store_true", default = FALSE, 
+                help = "Don't print any status messages", metavar = "QUIET"),
+	make_option(c("-d", "--debug"), action = "store_true", default = FALSE, 
+                help = "Print extensive status messages", metavar = "DEBUG"),
+)
+
+# Parse args
+parser <- OptionParser(option_list = option_list)
+opts <- parse_args(parser)
+
+anno_file <- opts$anno
+output_dir <- opts$output
+
+# Import annotations
+anno = import(anno_file)
+txdb = makeTxDbFromGRanges(anno)
+
+### split into gene/exon/intron regions
+exon_tx <- exonsBy(txdb, by = "tx")
+intron_tx <- intronsByTranscript(txdb)
+
+meta_info <- transcripts(txdb)
+meta_gtf <- filtered_gtf[filtered_gtf$type == "transcript"]
+names(meta_gtf) <- meta_gtf$transcript_id
+meta_info$gene_id <- meta_gtf[meta_info$tx_name]$gene_id
+
+exon_tx <- exon_tx %>% unlist %>% granges()
+exon_tx$gene_id <- meta_info[names(exon_tx) %>% as.numeric()]$gene_id
+
+intron_tx <- intron_tx %>% unlist %>% granges()
+intron_tx$gene_id <- meta_info[names(intron_tx) %>% as.numeric()]$gene_id
+
+exon_intron <- c(exon_tx, intron_tx)
+exon_intron <- exon_intron %>% split(exon_intron$gene_id) %>% GRangesList()
+
+exon_intron <- exon_intron %>% disjoin() %>% unlist()
+exon_intron$class <- "intron"
+exon_intron$class[exon_intron %over% exon_tx] <- "exon"
+
+exon_gr <- exon_intron[exon_intron$class == "exon"]
+intron_gr <- exon_intron[exon_intron$class == "intron"]
+
+exon_gr <- exon_gr %>% split(names(exon_gr)) %>% GRangesList() %>% reduce()
+intron_gr <- intron_gr %>% split(names(intron_gr)) %>% GRangesList() %>% reduce()
+
+mean(intron_gr %over% exon_tx)
+mean(intron_tx %over% exon_tx)
+mean(exon_gr %over% intron_gr)
+mean(intron_gr %over% exon_gr)
+
+gene_gr <- exon_intron %>% split(names(exon_intron)) %>% GRangesList() %>% reduce()
+
+names(exon_gr) <- paste(names(exon_gr), "exon", sep = "_")
+names(intron_gr) <- paste(names(intron_gr), "intron", sep = "_")
+###
+
+saveRDS(gene_gr, file.path(output_dir, "gene_gr.rds"))
+saveRDS(exon_gr, file.path(output_dir, "exon_gr.rds"))
+saveRDS(intron_gr, file.path(output_dir, "intron_gr.rds"))
