318c01ade9e41bcad4c1aabf80ddecd24fdbdb1f
Truman Mooney <85713081+TrumanMooney@users.noreply.github.com>
2024-10-21 18:25:53 -0400

    Continue linear_mapping integration

--- scripts/build_R_references.R
+++ scripts/build_R_references.R
@@ -1,50 +1,77 @@
+
 library(magrittr)
 library(rtracklayer)
 library(GenomicFeatures)
 
-dir_path <- "/users/yzhong36/data_rsingh47/yzhong36/BP_prediction/lariat_simulation/linear_mapping/ref"
+# dir_path <- "/users/yzhong36/data_rsingh47/yzhong36/BP_prediction/lariat_simulation/linear_mapping/ref"
 
-# Check if the directory exists
-if (!file.exists(dir_path)) {
-  # Create the directory
-  dir.create(dir_path, recursive = TRUE)
-  message("Directory created: ", dir_path)
-} else {
-  message("Directory already exists: ", dir_path)
-}
+# # Check if the directory exists
+# if (!file.exists(dir_path)) {
+#   # Create the directory
+#   dir.create(dir_path, recursive = TRUE)
+#   message("Directory created: ", dir_path)
+# } else {
+#   message("Directory already exists: ", dir_path)
+# }
 
-### prepare annotation file
-gtf <- import("/users/yzhong36/data_rsingh47/yzhong36/BP_prediction/datasets/annotations/gencode/gencode.v44.basic.annotation.gtf.gz") 
-gtf
-table(gtf$gene_type)
+# ### prepare annotation file
+# gtf <- import("/users/yzhong36/data_rsingh47/yzhong36/BP_prediction/datasets/annotations/gencode/gencode.v44.basic.annotation.gtf.gz") 
+# gtf
+# table(gtf$gene_type)
 
-# BAD_GENE_TYPES = c('transcribed_unprocessed_pseudogene',
-#                    'unprocessed_pseudogene',
-#                    'processed_pseudogene' ,
-#                    'transcribed_processed_pseudogene' ,
-#                    'TEC',
-#                    'transcribed_unitary_pseudogene' ,
-#                    'rRNA_pseudogene',
-#                    'unitary_pseudogene' ,
-#                    'pseudogene' ,
-#                    'IG_V_pseudogene' ,
-#                    'TR_V_pseudogene' ,
-#                    'IG_C_pseudogene',
-#                    'TR_J_pseudogene' ,
-#                    'IG_J_pseudogene' ,
-#                    'IG_pseudogene',
-#                    'artifact' 
-#                    )
+# # BAD_GENE_TYPES = c('transcribed_unprocessed_pseudogene',
+# #                    'unprocessed_pseudogene',
+# #                    'processed_pseudogene' ,
+# #                    'transcribed_processed_pseudogene' ,
+# #                    'TEC',
+# #                    'transcribed_unitary_pseudogene' ,
+# #                    'rRNA_pseudogene',
+# #                    'unitary_pseudogene' ,
+# #                    'pseudogene' ,
+# #                    'IG_V_pseudogene' ,
+# #                    'TR_V_pseudogene' ,
+# #                    'IG_C_pseudogene',
+# #                    'TR_J_pseudogene' ,
+# #                    'IG_J_pseudogene' ,
+# #                    'IG_pseudogene',
+# #                    'artifact' 
+# #                    )
 
-BAD_GENE_TYPES <- c()
+# BAD_GENE_TYPES <- c()
 
-filtered_gtf <- gtf[!gtf$gene_type %in% BAD_GENE_TYPES]
-table(filtered_gtf$gene_type)
+# filtered_gtf <- gtf[!gtf$gene_type %in% BAD_GENE_TYPES]
+# table(filtered_gtf$gene_type)
 
-txdb <- makeTxDbFromGRanges(filtered_gtf)
-###
+# txdb <- makeTxDbFromGRanges(filtered_gtf)
+# ###
 
-### split gtf into gene/exon/intron regions
+# Get args
+args <- commandArgs(trailingOnly = TRUE)
+
+# Define arg options
+option_list <- list(
+    make_option(c("-a", "--anno"), type = "character", default = NULL, 
+                help = "input annotation file (GTF or GFF)", metavar = "ANNO_PATH"),
+	make_option(c("-o", "--output"), type = "character", default = NULL, 
+                help = "Path to output dir", metavar = "OUT_PATH"),
+	make_option(c("-d", "--quiet"), action = "store_true", default = FALSE, 
+                help = "Don't print any status messages", metavar = "QUIET"),
+	make_option(c("-d", "--debug"), action = "store_true", default = FALSE, 
+                help = "Print extensive status messages", metavar = "DEBUG"),
+)
+
+# Parse args
+parser <- OptionParser(option_list = option_list)
+opts <- parse_args(parser)
+
+anno_file <- opts$anno
+output_dir <- opts$output
+
+# Import annotations
+anno = import(anno_file)
+txdb = makeTxDbFromGRanges(anno)
+
+### split into gene/exon/intron regions
 exon_tx <- exonsBy(txdb, by = "tx")
 intron_tx <- intronsByTranscript(txdb)
 
@@ -83,6 +110,6 @@ names(exon_gr) <- paste(names(exon_gr), "exon", sep = "_")
 names(intron_gr) <- paste(names(intron_gr), "intron", sep = "_")
 ###
 
-saveRDS(gene_gr, file.path(dir_path, "gene_gr.rds"))
-saveRDS(exon_gr, file.path(dir_path, "exon_gr.rds"))
-saveRDS(intron_gr, file.path(dir_path, "intron_gr.rds"))
+saveRDS(gene_gr, file.path(output_dir, "gene_gr.rds"))
+saveRDS(exon_gr, file.path(output_dir, "exon_gr.rds"))
+saveRDS(intron_gr, file.path(output_dir, "intron_gr.rds"))
