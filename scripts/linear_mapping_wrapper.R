4621cd8974749bd3f438a174592561ddd7a5bd07
yzhong36 <yu_zhong@brown.edu>
2024-11-20 18:10:01 -0500

    Revert "filter_head_aligns.py: fix bp genomic context seq length"
    This reverts commit 45a39d4e9cc39ec3f0546195b4fab9b436ac029e.
    
    revert

--- scripts/linear_mapping_wrapper.R
+++ scripts/linear_mapping_wrapper.R
@@ -10,12 +10,12 @@ option_list <- list(
                 help = "input bam file", metavar = "BAM_PATH"),
     make_option(c("-f", "--file"), type = "character", default = NULL, 
                 help = "Path to utils R file", metavar = "FILE_PATH"),
-    make_option(c("-g", "--gtf"), type = "character", default = NULL, 
-                help = "Path to GTF dir", metavar = "DIR_PATH"),
-    make_option(c("-o", "--output"), type = "character", default = NULL, 
-                help = "Path to output dir", metavar = "OUT_PATH"),
-    make_option(c("-r", "--read_layout"), type = "character", default = "pairEnd", 
-                help = "singleEnd or pairEnd datasets", metavar = "READ_LAYOUT")
+    make_option(c("-r", "--ref_dir"), type = "character", default = NULL, 
+                help = "Path to reference dir", metavar = "DIR_PATH"),
+    make_option(c("-o", "--output_base"), type = "character", default = NULL, 
+                help = "Path to output", metavar = "OUT_PATH"),
+    make_option(c("-l", "--read_layout"), type = "character", default = "paired", 
+                help = "single = single-end sequencing data, paired = paired-end sequencing data", metavar = "READ_LAYOUT")
 )
 
 parser <- OptionParser(option_list = option_list)
@@ -23,12 +23,12 @@ opts <- parse_args(parser)
 
 input_bam <- opts$input
 utils_R <- opts$file
-gtf_dir <- opts$gtf
-output_dir <- opts$output
+ref_dir <- opts$ref_dir
+output_base <- opts$output_base
 read_layout <- opts$read_layout
 
-if (is.null(read_layout) || !read_layout %in% c("singleEnd", "pairEnd")) {
-    stop("--read_layout must be singleEnd or pairEnd.")
+if (is.null(read_layout) || !read_layout %in% c("single", "paired")) {
+    stop("--read_layout must be 'single' or 'paired'.")
 }
 
 ### Source utils
@@ -36,41 +36,20 @@ source(utils_R)
 ###
 
 ### Prepare annotation file
-gene_gr <- readRDS(file.path(gtf_dir, "gene_gr.rds"))
-exon_gr <- readRDS(file.path(gtf_dir, "exon_gr.rds"))
-intron_gr <- readRDS(file.path(gtf_dir, "intron_gr.rds"))
+gene_gr <- readRDS(file.path(ref_dir, "gene_gr.rds"))
+exon_gr <- readRDS(file.path(ref_dir, "exon_gr.rds"))
+intron_gr <- readRDS(file.path(ref_dir, "intron_gr.rds"))
 ###
 
 ### Run counting step
-singleEnd_mode <- ifelse(read_layout == "pairEnd", F, T)
+singleEnd_mode <- ifelse(read_layout == "paired", F, T)
 bam_res <- linear_map_integrate(input_bam,
                                 gene_gr = gene_gr, exon_gr = exon_gr, intron_gr = intron_gr,
                                 singleEnd = singleEnd_mode)
-
-n_reads <- total_reads_bam(input_bam, 
-                           isPaired = !singleEnd_mode, 
-                           isFirstMateRead = ifelse(!singleEnd_mode, T, NA))
-summary_table <- bam_res[,-1] %>% colSums()
-summary_table <- cbind(summary_table %>% data.frame() %>% t, 
-                       "intergenic_ambiguous" = n_reads - summary_table["gene"],
-                       "spliced" = summary_table["exon_exon_junc"],
-                       "unspliced" = summary_table["exon_intron_junc"] + summary_table["intron_only"])
 ###
 
 ### Output counts table
-# Check if the directory exists
-if (!file.exists(output_dir)) {
-  # Create the directory
-  dir.create(output_dir, recursive = TRUE)
-  message("Directory created: ", output_dir)
-} else {
-  message("Directory already exists: ", output_dir)
-}
 
-write.table(bam_res, file.path(output_dir, 
-                               paste0(basename(input_bam), "_", "count.tsv")),
-            row.names = F, col.names = T, quote = F, sep = "\t")
-write.table(summary_table, file.path(output_dir, 
-                                     paste0(basename(input_bam), "_", "summary_count.tsv")),
+write.table(bam_res, paste0(output_base, "output.bam_count.tsv"),
             row.names = F, col.names = T, quote = F, sep = "\t")
 ###
