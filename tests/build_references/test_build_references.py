1fab430ceb896dbec1d36b4cfb1c2529a7341287
Truman Mooney <85713081+TrumanMooney@users.noreply.github.com>
2024-12-05 14:07:22 -0500

    build_references, test_build_references: fix reference-build test and add testing for build_R_refs script

--- tests/build_references/test_build_references.py
+++ tests/build_references/test_build_references.py
@@ -1,70 +1,106 @@
-# import gzip
-# import os
-# import pathlib
-# import shutil
-# import subprocess
-# import sys
+import gzip
+import hashlib
+import os
+import pathlib
+import shutil
+import subprocess
+import sys
 
-# import pytest
+import pytest
 
-# sys.path.append(str(pathlib.Path(__file__).parent.parent.resolve()))
-# import test_utils
-
-
-# # =============================================================================#
-# #                                  Globals                                     #
-# # =============================================================================#
-# PACKAGE_DIR = pathlib.Path(__file__).parent.parent.parent.resolve()
-# BUILD_REFERENCES_DIR = PACKAGE_DIR/'tests'/'build_references'
-# INPUTS_DIR = PACKAGE_DIR/'tests'/'inputs'
-# OUTPUTS_DIR = PACKAGE_DIR/'tests'/'outputs'
+sys.path.append(str(pathlib.Path(__file__).parent.parent.resolve()))
+import test_utils
 
 
+# =============================================================================#
+#                                  Globals                                     #
+# =============================================================================#
+PACKAGE_DIR = pathlib.Path(__file__).parent.parent.parent.resolve()
+BUILD_REFERENCES_DIR = PACKAGE_DIR/'tests'/'build_references'
+INPUTS_DIR = PACKAGE_DIR/'tests'/'inputs'
+OUTPUTS_DIR = PACKAGE_DIR/'tests'/'outputs'
 
-# # =============================================================================#
-# #                                   Tests                                      #
-# # =============================================================================#
-# # Required args + the transcript attribute and gene attribute args since their values depend on the genome gtf
-# @pytest.mark.parametrize('base',
-# 					[f"-f {BUILD_REFERENCES_DIR/'inputs'/'genome.fa'} -i {BUILD_REFERENCES_DIR/'inputs'/'hisat2_index'} -x tid -g gid", 
-# 					f"--genome_fasta {BUILD_REFERENCES_DIR/'inputs'/'genome.fa'}  --hisat2_index {BUILD_REFERENCES_DIR/'inputs'/'hisat2_index'} --transcript_attribute tid --gene_attribute gid",],)
-# @pytest.mark.parametrize('anno',
-# 					[f"-a {BUILD_REFERENCES_DIR/'inputs'/'anno.gtf'}", 
-# 					f"-a {BUILD_REFERENCES_DIR/'inputs'/'anno.gff'}", 
-# 					f"--genome_anno {BUILD_REFERENCES_DIR/'inputs'/'anno.gtf'}"])
-# # Optional args
-# @pytest.mark.parametrize('repeatmasker_bed',
-# 					[None, 
-# 					f"-r {BUILD_REFERENCES_DIR/'inputs'/'repeatmasker.bed'}", 
-# 					f"--repeatmasker_bed {BUILD_REFERENCES_DIR/'inputs'/'repeatmasker.bed'}"])
-# @pytest.mark.parametrize('threads',
-# 					[None, '-t 4', '--threads 4'])
-# @pytest.mark.parametrize('copy',
-# 					[None, '--copy'])
-# def test_build_references(base, anno, repeatmasker_bed, threads, copy, tmp_path):
-# 	command = f"python {PACKAGE_DIR/'build_references.py'} {base} {anno} -o {tmp_path}"
-# 	for optional_arg in (repeatmasker_bed, threads, copy):
-# 		if optional_arg is not None:
-# 			command = f'{command} {optional_arg}'
+# =============================================================================#
+#                                   Tests                                      #
+# =============================================================================#
+# Required anno arg + the transcript attribute and gene attribute args since their values depend on the anno file
+@pytest.mark.parametrize('anno',
+					[f"--genome_anno {BUILD_REFERENCES_DIR/'inputs'/'anno.gtf'} --t_attr tid --g_attr gid", 
+					f"--genome_anno {BUILD_REFERENCES_DIR/'inputs'/'anno.gff'} --t_attr tid --g_attr gid"])
+# Optional args
+@pytest.mark.parametrize('repeatmasker_bed',
+					[None, 
+	  				f"--repeatmasker_bed {BUILD_REFERENCES_DIR/'inputs'/'repeatmasker.bed'}"])
+@pytest.mark.parametrize('threads',
+					[None, 
+	  				'--threads 1',
+	  				'--threads 4'])
+@pytest.mark.parametrize('copy',
+					[None, 
+	  				'--copy'])
+@pytest.mark.parametrize('verbosity',
+					[None, 
+	  				'--quiet',
+					'--debug'])
+def test_build_references(anno, repeatmasker_bed, threads, copy, verbosity, tmp_path):
+	command = f"python {PACKAGE_DIR/'build_references.py'} --skip_r" +\
+			f" --genome_fasta {BUILD_REFERENCES_DIR/'inputs'/'genome.fa'}" +\
+			f" --hisat2_index {BUILD_REFERENCES_DIR/'inputs'/'hisat2_index'}" +\
+			f" {anno} -o {tmp_path}"
+	for optional_arg in (repeatmasker_bed, threads, copy):
+		if optional_arg is not None:
+			command += ' ' + optional_arg
 
-# 	response = subprocess.run(command, shell=True, capture_output=True, text=True)
-# 	response_text = '\n' + response.stdout + response.stderr
-# 	assert response.returncode == 0, response_text
+	response = subprocess.run(command, shell=True, capture_output=True, text=True)
+	response_text = '\n' + response.stdout + response.stderr
+	assert response.returncode == 0, response_text
 
-# 	# Check output
-# 	for ref, out in (
-# 					# (BUILD_REFERENCES_DIR/'outputs'/'exons.tsv', tmp_path/'exons.tsv.gz'),
-# 					(BUILD_REFERENCES_DIR/'outputs'/'introns.tsv', tmp_path/'introns.tsv.gz'),
-# 					(BUILD_REFERENCES_DIR/'outputs'/'fivep_sites.fa', tmp_path/'fivep_sites.fa')
-# 					):
-# 		ref_lines, out_lines = test_utils.load_file_lines(ref, out)
-# 		if ref_lines == out_lines:
-# 			continue
+	# Check output
+	for ref, out in (
+					(BUILD_REFERENCES_DIR/'outputs'/'introns.tsv', tmp_path/'introns.tsv.gz'),
+					(BUILD_REFERENCES_DIR/'outputs'/'fivep_sites.fa', tmp_path/'fivep_sites.fa')
+					):
+		ref_lines, out_lines = test_utils.load_file_lines(ref, out)
+		if ref_lines == out_lines:
+			continue
 
-# 		# If file contents differ, decide how to report
-# 		if test_utils.vscode_available():
-# 			test_utils.vscode_compare(ref, out)
-# 			pytest.fail(f'Output file differs from expected output: {out.name}')
-# 		else:
-# 			assert ref_lines == out_lines
-# 		pass
+		# If file contents differ, decide how to report
+		if test_utils.vscode_available():
+			test_utils.vscode_compare(ref, out)
+			pytest.fail(f'Output file differs from expected output: {out.name}')
+		else:
+			assert ref_lines == out_lines
+		pass
+
+
+# Optional args
+@pytest.mark.parametrize('verbosity',
+					[None, 
+	  				'--quiet',
+					'--debug'])
+def test_build_R_refs(verbosity, tmp_path):
+	command = f"Rscript {PACKAGE_DIR}/scripts/build_R_refs.R" +\
+			f" --anno {BUILD_REFERENCES_DIR/'inputs'/'hg38.gencode.v44.sample.gtf'}" +\
+			f" --g_attr gene_id --t_attr transcript_id --output {tmp_path}"
+	for optional_arg in (verbosity,):
+		if optional_arg is not None:
+			command += ' ' + optional_arg
+	response = subprocess.run(command, shell=True, capture_output=True, text=True)
+	response_text = '\n' + response.stdout + response.stderr
+	if response.returncode != 0:
+		pytest.fail(response_text)
+			
+	for ref, out in (
+					(BUILD_REFERENCES_DIR/'outputs'/'exon_gr.rds', tmp_path/'exon_gr.rds'),
+					(BUILD_REFERENCES_DIR/'outputs'/'intron_gr.rds', tmp_path/'intron_gr.rds'),
+					(BUILD_REFERENCES_DIR/'outputs'/'gene_gr.rds', tmp_path/'gene_gr.rds'),
+					):
+		
+		with open(ref, 'rb') as rb:
+			ref_md5 = hashlib.md5(rb.read()).hexdigest()
+		with open(out, 'rb') as rb:
+			out_md5 = hashlib.md5(rb.read()).hexdigest()
+
+		if ref_md5 != out_md5:
+			pytest.fail(f'Output file differs from expected output: {out.name}\n' +\
+			   			f'{ref_md5} (ref) != {out_md5} (out)')
--- tests/filter_fivep_aligns/test_filter_fivep_aligns.py
+++ tests/filter_fivep_aligns/test_filter_fivep_aligns.py
@@ -41,8 +41,9 @@ def test_filter_fivep_aligns(threads, prefix, strand, tmp_path):
 			  f" {FILTER_FIVEP_ALIGNS_DIR/'inputs'/'genome.fa'} {FILTER_FIVEP_ALIGNS_DIR/'inputs'/'fivep_sites.fa'} {strand} {threads}"
 	response = subprocess.run(command, shell=True, capture_output=True, text=True)
 	response_text = '\n' + response.stdout + response.stderr
-	assert response.returncode == 0, response_text
-
+	if response.returncode != 0:
+		pytest.fail(response_text)
+		
 	# Check output
 	for ref, out in ((FILTER_FIVEP_ALIGNS_DIR/'outputs'/'failed_fivep_alignments.tsv', tmp_path/f'{prefix}failed_fivep_alignments.tsv'),
 				  	(FILTER_FIVEP_ALIGNS_DIR/'outputs'/'tails.tsv', tmp_path/f'{prefix}tails.tsv'),
