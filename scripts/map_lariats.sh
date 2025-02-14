3dca3a7d890dfdb43e7a39b8bdb0746161817373
Truman Mooney <85713081+TrumanMooney@users.noreply.github.com>
2025-02-14 17:38:53 -0500

    map_lariats: wrap printf messages in function

--- scripts/map_lariats.sh
+++ scripts/map_lariats.sh
@@ -87,8 +87,14 @@ temp_files=(
 #=============================================================================#
 #                                  Functions                                  #
 #=============================================================================#
+print_message(){
+	text="$1"
+	out_msg="$(date +'%d/%b/%Y %H:%M:%S') | $text \n"
+	printf "$out_msg"
+}
+
 delete_temp_files(){
-	printf "$(date +'%d/%b/%Y %H:%M:%S') | Deleting temporary files...\n"
+	print_message "Deleting temporary files..."
 	for file in "${temp_files[@]}"; do
 		if [ $file == $OUTPUT_BASE"read_classes.tsv.gz" ] & $KEEP_CLASSES; then
 			continue
@@ -102,21 +108,21 @@ delete_temp_files(){
 
 end_run() {
 	### Classify reads
-	printf "$(date +'%d/%b/%Y %H:%M:%S') | Classifying linearly-aligned reads...\n"
+	print_message "Classifying linearly-aligned reads..."
 	Rscript $PIPELINE_DIR/scripts/linear_mapping_wrapper.R -i $output_bam -f $PIPELINE_DIR/scripts/linear_mapping.R -r $REF_DIR -l $SEQ_TYPE -o $OUTPUT_BASE
 	check_exitcode
-	printf "$(date +'%d/%b/%Y %H:%M:%S') | Classifying non-linearly-aligned reads...\n"
+	print_message "Classifying non-linearly-aligned reads..."
 	python -u $PIPELINE_DIR/scripts/classify_nonlinear.py $OUTPUT_BASE $SEQ_TYPE $LOG_LEVEL 
 	check_exitcode
 
 	### Summarise results
 	### Also create lariat, circ, and temp-switch output files if they don't exist
-	printf "$(date +'%d/%b/%Y %H:%M:%S') | Summarising results...\n"
+	print_message "Summarising results..."
 	python -u $PIPELINE_DIR/scripts/summarise.py $OUTPUT_BASE $LOG_LEVEL $SEQ_TYPE
 	check_exitcode
 
 	### Create a subdir named "plots" and make summary plots in it
-	printf "$(date +'%d/%b/%Y %H:%M:%S') | Building plots...\n"
+	print_message "Building plots..."
 	Rscript $PIPELINE_DIR/scripts/summary_plots.R -o $OUTPUT_BASE
 	check_exitcode
 
@@ -128,9 +134,9 @@ end_run() {
 
 	### Print completion message
 	if [ "${OUTPUT_BASE: -1}" == "/" ];then
-		printf "$(date +'%d/%b/%Y %H:%M:%S') | Lariat mapping complete.\n"
+		print_message "Lariat mapping complete."
 	else
-		printf "$(date +'%d/%b/%Y %H:%M:%S') | Lariat mapping complete for "$(echo ${OUTPUT_BASE:0:-1} | sed "s:.*/::")".\n"
+		print_message "Lariat mapping complete for "$(echo ${OUTPUT_BASE:0:-1} | sed "s:.*/::")"."
 	fi
 
 	### Exit
@@ -145,7 +151,7 @@ check_exitcode() {
 	### Exit code 4 = No reads or alignments remain for processing part-way through the pipeline
 	### End the run early
 	elif [ $exit_code -eq 4 ];then
-		printf "$(date +'%d/%b/%Y %H:%M:%S') | No reads remaining.\n"
+		print_message "No reads remaining."
 		end_run
 	else
 		exit $exit_code
@@ -158,7 +164,7 @@ check_exitcode() {
 #                                    Calls                                    #
 #=============================================================================#
 ### Map filtered reads to genome and keep unmapped reads. Lariat reads crossing the brachpoint will not be able to map to the gene they're from
-printf "$(date +'%d/%b/%Y %H:%M:%S') | Mapping reads to genome...\n"
+print_message "Mapping reads to genome..."
 if [ "$SEQ_TYPE" == "single" ]; then
 	# Map
 	hisat2 --no-softclip -k 1 --max-seeds 20 --pen-noncansplice 0 --n-ceil L,0,0.05 --score-min L,0,-0.24 --bowtie2-dp 1 \
@@ -190,21 +196,21 @@ check_exitcode
 
 unmapped_read_count=$(samtools view --count --require-flags 4 $output_bam)
 if [ $unmapped_read_count == 0 ];then
-	printf "$(date +'%d/%b/%Y %H:%M:%S') | All reads mapped linearly to genome.\n"
+	print_message "All reads mapped linearly to genome."
 	end_run
 fi
 
 
 ### Create fasta file of unmapped reads 
-printf "$(date +'%d/%b/%Y %H:%M:%S') | Creating fasta file of unmapped reads...\n"
+print_message "Creating fasta file of unmapped reads..."
 samtools fasta -N --require-flags 4 -o $unmapped_fasta $output_bam >/dev/null 2>&1
 check_exitcode
-printf "$(date +'%d/%b/%Y %H:%M:%S') | Indexing unmapped reads fasta file...\n"
+print_message "Indexing unmapped reads fasta file..."
 samtools faidx $unmapped_fasta 
 check_exitcode
 
 ### Build a bowtie2 index of the unmapped reads
-printf "$(date +'%d/%b/%Y %H:%M:%S') | Building bowtie2 index of unmapped fasta...\n"
+print_message "Building bowtie2 index of unmapped fasta..."
 bowtie2-build --quiet --threads $THREADS $unmapped_fasta $unmapped_fasta
 check_exitcode 
 
@@ -212,7 +218,7 @@ check_exitcode
 ## Align fasta file of all 5' splice sites (first 20nts of introns) to unmapped reads index
 # We need to order the output SAM by read_id (which is the reference in this case)
 # so we can process all the alignments to each read together in the following filtering 
-printf "$(date +'%d/%b/%Y %H:%M:%S') | Mapping 5' splice sites to reads...\n"
+print_message "Mapping 5' splice sites to reads..."
 bowtie2 --end-to-end --sensitive --no-unal -f -k 10000 --score-min C,0,0 --threads $THREADS -x $unmapped_fasta -U $FIVEP_FASTA \
 	> $fivep_to_reads.tmp
 check_exitcode
@@ -223,7 +229,7 @@ check_exitcode
 
 
 ## Extract reads with a mapped 5' splice site and trim it off
-printf "$(date +'%d/%b/%Y %H:%M:%S') | Finding 5' read alignments and trimming reads...\n"
+print_message "Finding 5' read alignments and trimming reads..."
 # scalene --html --outfile "$OUTPUT_BASE"filter_fivep_aligns.html \
 # 	$PIPELINE_DIR/scripts/filter_fivep_aligns.py $OUTPUT_BASE $LOG_LEVEL $GENOME_FASTA $FIVEP_FASTA $STRAND $THREADS
 python -u $PIPELINE_DIR/scripts/filter_fivep_aligns.py $OUTPUT_BASE $LOG_LEVEL $GENOME_FASTA $FIVEP_FASTA $STRAND $THREADS
@@ -231,7 +237,7 @@ check_exitcode
 
 
 ### Map read heads to genome
-printf "$(date +'%d/%b/%Y %H:%M:%S') | Mapping heads to genome...\n"
+print_message "Mapping heads to genome..."
 hisat2 --no-softclip --no-spliced-alignment --very-sensitive -k 100 \
 	   --no-unal --threads $THREADS -f -x $GENOME_INDEX -U $heads_fasta \
 	> $heads_to_genome.tmp
@@ -244,22 +250,22 @@ check_exitcode
 
 
 ### Filter head alignments
-printf "$(date +'%d/%b/%Y %H:%M:%S') | Analyzing head alignments and outputting lariat table...\n"
+print_message "Analyzing head alignments and outputting lariat table..."
 # scalene --html --outfile "$OUTPUT_BASE"filter_head_aligns.html \
-# 	$PIPELINE_DIR/scripts/filter_head_aligns.py $THREADS $INTRONS_TSV $GENOME_FASTA $OUTPUT_BASE $LOG_LEVEL 
+# 	$PIPELINE_DIR/scripts/filter_head_aligns.py $THREADS $EXONS_TSV $INTRONS_TSV $GENOME_FASTA $TEMP_SWITCH_FILTER $OUTPUT_BASE $LOG_LEVEL 
 python -u $PIPELINE_DIR/scripts/filter_head_aligns.py $THREADS $EXONS_TSV $INTRONS_TSV $GENOME_FASTA $TEMP_SWITCH_FILTER $OUTPUT_BASE $LOG_LEVEL 
 check_exitcode
 
 
 ### Filter lariat mappings and choose 1 for each read
-printf "$(date +'%d/%b/%Y %H:%M:%S') | Filtering putative lariat alignments...\n"
+print_message "Filtering putative lariat alignments..."
 python -u $PIPELINE_DIR/scripts/filter_lariats.py $OUTPUT_BASE $LOG_LEVEL $SEQ_TYPE $GENOME_FASTA $REPEATS_BED
 check_exitcode
 
 
 ### Correct branchpoint positions, if extra files are provided
 if ! [ "$PWM_FILES" == "" ]; then
-	printf "$(date +'%d/%b/%Y %H:%M:%S') | Correcting branchpoints with PWMs...\n"
+	print_message "Correcting branchpoints with PWMs..."
 	Rscript $PIPELINE_DIR/scripts/bp_correction_wrapper.R \
 		--input "$OUTPUT_BASE"lariat_reads.tsv \
 		--ref_fasta $GENOME_FASTA \
@@ -270,7 +276,7 @@ if ! [ "$PWM_FILES" == "" ]; then
 		--output_base $OUTPUT_BASE 
 	check_exitcode
 elif ! [ "$MODEL_FILE" == "" ]; then
-	printf "$(date +'%d/%b/%Y %H:%M:%S') | Correcting branchpoints with DeepEnsemble...\n"
+	print_message "Correcting branchpoints with DeepEnsemble..."
 	Rscript $PIPELINE_DIR/scripts/bp_correction_wrapper.R \
 		--input "$OUTPUT_BASE"lariat_reads.tsv \
 		--ref_fasta $GENOME_FASTA \
@@ -285,7 +291,7 @@ fi
 
 ### Make a custom track BED file of identified lariats 
 if $UCSC_TRACK; then
-	printf "$(date +'%d/%b/%Y %H:%M:%S') | Making UCSC Genome Browser track...\n"
+	print_message "Making UCSC Genome Browser track..."
 	python -u $PIPELINE_DIR/scripts/make_track.py $OUTPUT_BASE $LOG_LEVEL 
 	check_exitcode
 fi
